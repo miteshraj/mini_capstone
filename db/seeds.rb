ecf53fba67843d73f3c9cc947f377781466bf167
miteshraj <miteshraj92@gmail.com>
2018-01-16 20:09:09 -0800

    Selling Products app

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+soap = Product.new(title: "Soap", price: "3.99", image_url: "https://img.makeupalley.com/0/4/5/7/2282391.JPG", 
+	description: "A fancy bar of Dove soap") 
+
+cheetos = Product.new(title: "Hot Cheeto Fries", price: "1.99", image_url: "https://www.google.com/url?sa=i&rct=j&q=&esrc=s&source=images&cd=&ved=&url=http%3A%2F%2Forigin-www.fritolay.com%2Fimages%2Fdefault-source%2Fblue-bag-image%2Fchesters-fries-flamin-hot.png%3Fsfvrsn%3D3c5f573a_4&psig=AOvVaw3VLHUZPb-J5teT5V_Nvdj-&ust=1516247700876505"
+	description: "Hottest Cheetos ever.....")
+
+sponges = Product.new(title: "Yellow Sponge", price: "999.00", image_url: "http://4.bp.blogspot.com/_x2M_h1XfLHE/SwWdg6BZgJI/AAAAAAAAAs4/8qugxsnCc4M/s1600/DSCF0853_p.jpg"
+	description: "Almost as cool as the real thing but not really")
+
